361d25c877da07b714f7f4c256740237d284817f
CJ Harries <cj@wizardsoftheweb.pro>
2017-06-18 17:51:59 -0500

    Break everything down so it can be tested
    Originally I just wanted to parse the README TOC, but I had a ton of insanely huge functions. As I was splitting those up, I
    realized I should probably pull this into its own module. I will probably do that eventually. But for now, each function is
    easily testable. I don't have tests for them yet, because, at the moment, I'm the only one with the symlink.

--- git-hooks/pre-commit.js
+++ git-hooks/pre-commit.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// TODO: pull this out into its own module
+
 let Bluebird, chalk, fsb, toc, path, shelljs;
 try {
     Bluebird = require("bluebird");
@@ -8,74 +10,144 @@ try {
     toc = require("markdown-toc");
     path = require("path");
     shelljs = require("shelljs");
+    log("Starting");
 } catch (error) {
-    console.log(chalk.red.bold("You must run 'npm install' before the pre-commit hook will run"));
+    console.error(`[${timestamp()}][pre-commit hook]: You must run 'npm install' before the pre-commit hook will run`);
     process.exit(1);
 }
 
-shelljs.echo(chalk.green("\nRunning pre-commit hook..."));
 shelljs.set("-e");
+Bluebird.promisifyAll(fsb, { suffix: "Bluebird" });
 
-Bluebird.promisifyAll(fsb, {suffix: "Bluebird"});
+const tocRegExp = /<!-- toc -->\n?([\s\S]*)<!-- tocstop -->/gi;
 
 const rootDirectory = path.join(__dirname, "..");
 shelljs.cd(rootDirectory);
 
-function checkIfFileIsStaged(filename) {
-    try {
-        shelljs.exec(`git diff --name-only --cached | grep '${filename}'`, {silent: true});
-        return true;
-    } catch (error) {
-        return false;
-    }
-}
-
 let exitCode = 0;
-
-function checkAndRebuildReadmeToc() {
-    const readmePath = path.join(rootDirectory, "README.md");
-    if (!shelljs.test("-f", readmePath)) {
-        shelljs.echo(chalk.dim("\tNo README found"));
-        return Bluebird.resolve();
-    }
-    if (!checkIfFileIsStaged("README.md")) {
-        shelljs.echo(chalk.dim("\tREADME not staged"));
-        return Bluebird.resolve();
-    }
-    return fsb.readFileBluebird(readmePath, "utf-8")
-        .then((contents) => {
-            const tocRegExp = /<!-- toc -->\n?([\s\S]*)<!-- tocstop -->/gi;
-            const searchContents = tocRegExp.exec(contents);
-            if (searchContents && searchContents[1]) {
-                const found = searchContents[1];
-                const built = `${toc(contents).content}\n`;
-                if (found !== built) {
-                    shelljs.echo(chalk.dim("\tRebuilding README TOC"));
-                    return fsb.writeFileBluebird(
-                            readmePath,
-                            contents.replace(
-                                tocRegExp,
-                                `<!-- toc -->\n${built}<!-- tocstop -->`
-                            ),
-                            "utf-8"
-                        );
-                }
-            }
-            shelljs.echo(chalk.dim("\tNo changes to README TOC"));
-        })
-        .then(() => {
-            shelljs.exec(`git add ${readmePath}`);
-        });
-}
 
 checkAndRebuildReadmeToc()
     .catch((error) => {
-        shelljs.echo(chalk.red.bold("Error in pre-commit hook:"));
-        shelljs.echo(error.message);
+        log(error.message, true);
         exitCode = 1;
     })
     .finally(() => {
-        const color = exitCode !== 0 ? chalk.red.bold : chalk.green;
-        shelljs.echo(color("pre-commit hook finished\n"));
+        log("Finished\n", exitCode);
         shelljs.exit(exitCode);
     });
+
+function timestamp() {
+    return (new Date()).toTimeString().substring(0, 8);
+}
+
+function log(message, isError) {
+    const color = isError ? chalk.red.bold : chalk.green;
+    shelljs.echo(
+        chalk.white(`[${timestamp()}]`) + (isError ? chalk.red.bold : chalk.green)("[pre-commit] ") + message
+    );
+}
+
+function checkIfFileIsStaged(filename) {
+    return new Bluebird((resolve, reject) => {
+        try {
+            shelljs.exec(`git diff --name-only --cached | grep '${filename}'`, { silent: true });
+            return resolve();
+        } catch (error) {
+            return reject(chalk.dim(`${filename} not staged; skipping`));
+        }
+    });
+}
+
+function checkIfFileExists(filepath, shortName) {
+    return new Bluebird((resolve, reject) => {
+        if (shelljs.test("-f", filepath)) {
+            return resolve();
+        }
+        return reject(chalk.dim(`${shortName ? shortName : path} not found; skipping`));
+    });
+}
+
+function loadFile(filepath, shortName) {
+    return checkIfFileExists(filepath, shortName)
+        .then(() => {
+            return checkIfFileIsStaged(shortName);
+        })
+        .then(() => {
+            return fsb.readFileBluebird(filepath, "utf-8")
+                .catch((error) => {
+                    // The file exists but cannot be opened
+                    error.isError = true;
+                    return Bluebird.reject(error);
+                });
+        });
+}
+
+function writeFile(filepath, contents, shortName) {
+    log(chalk.dim(`Rebuilding ${shortName ? shortName : filepath}`));
+    return fsb.writeFileBluebird(filepath, contents, "utf-8")
+        .catch((error) => {
+            error.isError = true;
+            return Bluebird.reject(error);
+        })
+}
+
+function stageFile(filepath, shortName) {
+    return new Bluebird((resolve, reject) => {
+        try {
+            log(chalk.dim(`Restaging ${shortName ? shortName : filepath}`));
+            shelljs.exec(`git add ${filepath}`);
+            return resolve();
+        } catch (error) {
+            error.isError = true;
+            return reject(error);
+        };
+    });
+}
+
+function findToc(contents) {
+    return new Bluebird((resolve, reject) => {
+        const searchContents = tocRegExp.exec(contents);
+        if (searchContents) {
+            return resolve([searchContents[1], `${toc(contents).content}\n`]);
+        } else {
+            return reject(chalk.dim("README TOC comments not found; skipping"));
+        }
+    });
+}
+
+function rejectUnchangedTocs([found, built]) {
+    return new Bluebird((resolve, reject) => {
+        if (found === built) {
+            return reject(chalk.dim("No README TOC changes"));
+        }
+        return resolve();
+    });
+}
+
+function parseReadmeToc(contents) {
+    return findToc(contents)
+        .then(rejectUnchangedTocs)
+        .then(() => {
+            return contents.replace(tocRegExp, `<!-- toc -->\n${built}<!-- tocstop -->`);
+        });
+}
+
+function checkAndRebuildReadmeToc() {
+    const readmePath = path.join(rootDirectory, "README.md");
+    let isError = false;
+    let readmeContents;
+    return loadFile(readmePath, "README")
+        .then(parseReadmeToc)
+        .then((contents) => {
+            return writeFile(readmePath, contents);
+        })
+        .then(() => {
+            return stageFile(readmePath, "README");
+        })
+        .catch((error) => {
+            if (error.isError) {
+                return Bluebird.reject(error);
+            }
+            log(error);
+        });
+}
