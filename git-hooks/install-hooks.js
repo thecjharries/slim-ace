144db32a9d140d0aad61c855a095d7efd8baa262
CJ Harries <cj@wizardsoftheweb.pro>
2017-06-18 22:53:19 -0500

    Add winston as a logging method

--- git-hooks/install-hooks.js
+++ git-hooks/install-hooks.js
@@ -2,18 +2,91 @@ const chalk = require("chalk");
 const path = require("path");
 const readline = require('readline');
 const shelljs = require("shelljs");
-shelljs.set("-e");
+const winston = require("winston");
 
+// Build variables for winston usage
+// TODO: modularize this
+
+// Determine the script's name
+const scriptName = path.basename(__filename).replace(/\.\w+$/, "");
+// const scriptName = __filename.replace(/^.*[\/\\]([^\/\\]+)\.\w+$/, "$1");
+// Create logs dir if DNE
+shelljs.mkdir("-p", "./logs");
+// Build logger with transports
+const logger = new winston.Logger({
+    level: "info",
+    transports: [
+          new (winston.transports.Console)({
+              colorize: true,
+              formatter: consoleFormatter,
+              level: "silly",
+              timestamp: true
+          })
+        , new (winston.transports.File)({
+            filename: path.join("logs", scriptName + ".log")
+          })
+    ]
+});
+// Assign color values
+const colorLevels = [
+    chalk.bold.red,
+    chalk.yellow.bold,
+    chalk.green,
+    chalk.cyan,
+    chalk.dim,
+    chalk.magenta
+];
+// Add named level aliases
+colorLevels.error = colorLevels[0];
+colorLevels.warn = colorLevels[1];
+colorLevels.info = colorLevels[2];
+colorLevels.verbose = colorLevels[3];
+colorLevels.debug = colorLevels[4];
+colorLevels.silly = colorLevels[5];
+
+// Wrap `toISOString` in case I decide to change it
+function timestamp() {
+    return (new Date).toISOString();
+}
+
+// Determine color for `level` or return no color for unknowns
+function colorize(level) {
+    const parsedLevel = level.toLowerCase();
+    if (colorLevels[parsedLevel]) {
+        return colorLevels[parsedLevel];
+    }
+    return String;
+}
+
+// Log colorized script name
+function specifyLogScript(options) {
+    return (options.colorize ? colorize(options.level) : String)(`[${scriptName}]`);
+}
+
+// Format the console transport
+// Example (pretend `install-hooks` is colored):
+// `[2017-06-19T03:41:10.732Z][install-hooks] Nothing entered`
+function consoleFormatter(options) {
+    return `[${timestamp()}]${specifyLogScript(options)} ${options.message}`;
+}
+
+// Run installation
 shelljs.echo(chalk.bold.red("\nWARNING: THESE HOOKS HAVE NOT BEEN RIGOROUSLY TESTED. USE AT YOUR OWN RISK."));
 
+// Get all hooks
 const hookFiles = shelljs.ls(__dirname);
 
+// Built prompt
+// Literally taken from `readline` docs
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// Make sure user wants to make bad decisions
 rl.question(`Type ${chalk.black.bgGreen("SOFT")} (ln -s) for careful linking or ${chalk.black.bgRed("FORCE")} (ln -sf) for brutal linking:\n`, (answer) => {
+    // Force an empty line after input
+    // TODO: figure out if there's an option for this
     shelljs.echo();
     if (/SOFT|FORCE/.test(answer)) {
         if (installHooks(answer === "FORCE")) {
@@ -32,14 +105,10 @@ rl.question(`Type ${chalk.black.bgGreen("SOFT")} (ln -s) for careful linking or 
     rl.close();
 });
 
-function timestamp() {
-    return (new Date()).toTimeString().substring(0, 8);
-}
-
+// Wrap `winston.log` because it used to be something else
+// Also because I haven't completely decided yet
 function log(message, isCaution) {
-    shelljs.echo(
-        chalk.white(`[${timestamp()}]`) + (isCaution ? chalk.yellow.bold : chalk.green)("[install-hooks] ") + message
-    );
+    logger.log(isCaution ? "warn" : "info", message);
 }
 
 function installHooks(force) {
